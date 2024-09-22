3501b3b34fd8093f4e3af2bf7f34991f7e87cb40
chqs-git <franciscobarreiras.fb@gmail.com>
2024-09-22 13:12:14 +0100

    github page domain created. under development.

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { createSignal } from "solid-js";
+import solidLogo from "./assets/solid.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  /*
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src={viteLogo} class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://solidjs.com" target="_blank">
+      <img src={solidLogo} class="logo solid" alt="Solid logo" />
+    </a>
+  </div>
+  */
+  return (
+    <>
+      <h1>Chqs-git Domain</h1>
+      <div class="card">
+        <p>This page is still under development.</p>
+      </div>
+      <p class="read-the-docs">You can check my github to learn more</p>
+    </>
+  );
+}
+
+export default App;
